6587f3459d25371af4d1c136386a216908991f44
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-02-17 15:42:54 +0300

    FunctionField the automatism

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -1,12 +1,12 @@
-import { Datagrid, List, ReferenceField, TextField , SimpleList} from 'react-admin';
+import { Datagrid, List, ReferenceField, TextField , FunctionField} from 'react-admin';
 
 export const PostList = () => (
     <List>
         <Datagrid>
 
             <TextField source="id" />
-            <TextField source="title" label="zavatra" />
-            <TextField source="body" />
+            <TextField source="title" label="post title" />
+            <FunctionField label="Expert" render={(record)=> `${record.body.substring(0,50)}...`} />
             <ReferenceField source="userId" reference="users" />
         </Datagrid>
 
